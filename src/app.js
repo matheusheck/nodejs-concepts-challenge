8b22f21a53863314ef8917161a97d7e8b9c12c3e
Matheus Heck <matheus@conduite.tv>
2020-06-21 00:03:57 +0200

    removes body parser

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
 const repositories = [];
 
